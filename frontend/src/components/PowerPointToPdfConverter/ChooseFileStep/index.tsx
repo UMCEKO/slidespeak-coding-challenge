6d323c5c69f42451c95b1af8d13a72700db03439
UMCEKO <umutcevdetkocak@gmail.com>
2025-05-20 23:01:26 +0300

    feat(frontend): add error handling to choose file step; remove redundant use client flag

---
+++ frontend/src/components/PowerPointToPdfConverter/ChooseFileStep/index.tsx
@@ -0,0 +1,78 @@
+import {Dispatch, FC, SetStateAction, useState} from 'react';
+import {useDropzone} from 'react-dropzone';
+import UploadIcon from '@/icons/UploadIcon';
+import {CurrentStep} from "@/components/PowerPointToPdfConverter";
+
+type ChooseFileStepProps = {
+  setCurrentStep: Dispatch<SetStateAction<CurrentStep>>,
+  setFile: Dispatch<SetStateAction<null | File>>
+};
+
+enum ErrorState {
+  NONE,
+  TOO_MANY_FILES,
+  SIZE_LIMIT_EXCEEDED,
+  INVALID_FILE,
+  OTHER
+}
+
+export const ChooseFileStep: FC<ChooseFileStepProps> = ({
+                                                          setCurrentStep,
+                                                          setFile
+                                                        }) => {
+
+  const [error, setError] = useState<ErrorState>(ErrorState.NONE)
+  const { getRootProps, getInputProps } = useDropzone({
+    onDropAccepted: async (files, event) => {
+      let file = files[0]
+      setFile(file)
+      setCurrentStep(CurrentStep.CONFIRMATION)
+    },
+    onDropRejected: (fileRejections) => {
+      const rejection = fileRejections[0];
+      if (rejection) {
+        // Handle specific rejection reasons
+        if (rejection.errors.some(e => e.code === 'file-too-large')) {
+          setError(ErrorState.SIZE_LIMIT_EXCEEDED);
+        } else if (rejection.errors.some(e => e.code === 'file-invalid-type')) {
+          setError(ErrorState.INVALID_FILE);
+        } else if (rejection.errors.some(e => e.code === "too-many-files")) {
+          setError(ErrorState.TOO_MANY_FILES)
+        } else {
+          setError(ErrorState.OTHER);
+        }
+      }
+    },
+    accept: {
+      'application/vnd.ms-powerpoint': ['.ppt'],
+      'application/vnd.openxmlformats-officedocument.presentationml.presentation': ['.pptx']
+    },
+    maxFiles: 1,
+    maxSize: 50 * 1024 * 1024,
+  });
+
+  return (
+      <div
+          className="group cursor-pointer rounded-xl border border-dashed border-gray-400 bg-white px-6 py-16 w-[416px] min-w-[365px]"
+          {...getRootProps()}
+      >
+        <input {...getInputProps()} />
+        <div className="flex shrink-0 grow-0 flex-col items-center gap-2">
+          <div className="mb-2 rounded-full bg-gray-100 p-2">
+            <div className="grid place-items-center rounded-full bg-gray-200 p-2 [&>svg]:size-8">
+              <UploadIcon/>
+            </div>
+          </div>
+          <p className="text-xs/[18px] leading-8 text-gray-600">
+            Drag and drop a PowerPoint file to convert to PDF.
+          </p>
+          <button
+              type="button"
+              className="rounded-lg bg-blue-50 px-4 py-2.5 text-sm text-blue-700 transition-colors group-hover:bg-blue-100"
+          >
+            Choose file
+          </button>
+        </div>
+      </div>
+  );
+};
