5d357cf80a4bdfd1ac1b252593190946213a339c
UMCEKO <umutcevdetkocak@gmail.com>
2025-05-21 00:32:15 +0300

    feat(frontend): add environment utility for type-safe environment variable usage

---
+++ frontend/src/utils/env.ts
@@ -0,0 +1,14 @@
+import {z} from "zod";
+
+
+const EnvSchema = z.object({
+  NEXT_PUBLIC_API_URL: z.string().url()
+})
+
+const processEnv = {
+  NEXT_PUBLIC_API_URL: process.env["NEXT_PUBLIC_API_URL"]
+}
+
+const env = EnvSchema.parse(processEnv)
+
+export default env
