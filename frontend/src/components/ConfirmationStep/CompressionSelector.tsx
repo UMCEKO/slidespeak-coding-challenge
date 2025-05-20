9504d864838d16b74bd515a38197e332275b63a3
UMCEKO <umutcevdetkocak@gmail.com>
2025-05-20 19:55:11 +0300

    fix(frontend): switch from rounded-xl to rounded-lg for consistency

--- frontend/src/components/ConfirmationStep/CompressionSelector.tsx
+++ frontend/src/components/ConfirmationStep/CompressionSelector.tsx
@@ -7,7 +7,7 @@ type CompressionSelectorProps = {
 export const CompressionSelector: FC<CompressionSelectorProps> = ({isLoading}) => {
     return (isLoading
         ?
-        <div className="flex w-full items-center gap-2 rounded-xl border border-gray-300 p-4">
+        <div className="flex w-full items-center gap-2 rounded-lg border border-gray-300 p-4">
             <div className="size-7 animate-spin-pretty rounded-full border-4 border-solid border-t-blue-500"></div>
             <p className="text-sm text-gray-700">Compressing your file...</p>
         </div>
