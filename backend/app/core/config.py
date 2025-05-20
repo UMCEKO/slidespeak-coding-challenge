5e046ebe5afe4f235ccb16800fdd8185a82cb1a1
UMCEKO <umutcevdetkocak@gmail.com>
2025-05-20 06:07:58 +0300

    fix(backend): fix wrong default values for the settings class

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -14,22 +14,25 @@ class Settings(BaseSettings):
     CORS_ORIGINS: List[Union[str, AnyHttpUrl]] = ["*"]
 
     # S3 Configuration
-    S3_ACCESS_KEY: str = os.getenv("S3_ACCESS_KEY")
-    S3_SECRET_KEY: str = os.getenv("S3_SECRET_KEY")
-    S3_REGION: str = os.getenv("S3_REGION")
-    S3_ENDPOINT: str = os.getenv("S3_ENDPOINT", None)
-    S3_BUCKET_NAME: str = os.getenv("S3_BUCKET")
+    S3_ACCESS_KEY: str
+    S3_SECRET_KEY: str
+    S3_REGION: str
+    S3_ENDPOINT: str
+    S3_BUCKET_NAME: str
     S3_PRESIGNED_URL_EXPIRY: int = 3600
 
     # Redis
-    REDIS_URL: str = os.getenv("REDIS_URL")
+    REDIS_URL: str
+
+    #
+    UNOSERVER_URL: str
 
     # File storage settings
     MAX_FILE_SIZE_MB: int = 50
 
     # Other
-    PORT: int = int(os.getenv("PORT", "3000"))
-    HOST: str = os.getenv("HOST", "0.0.0.0")
+    PORT: int = 3000
+    HOST: str = "0.0.0.0"
 
 
 
