f69a4b265a09826b012b218b7879838902c2fe2c
UMCEKO <umutcevdetkocak@gmail.com>
2025-05-20 22:57:22 +0300

    feat(frontend): add figtree font, make background same as figma's

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,12 +1,21 @@
 import './globals.css';
+import {Figtree} from "next/font/google";
 
 export const metadata = {
   title: 'PowerPoint optimizer - SlideSpeak',
   description: 'Reduce the size of your PowerPoint file without losing quality.',
 };
 
+const figtree = Figtree({
+    subsets: ['latin'],
+    // If you need specific weights (check what's available)
+    weight: ['400', '500', '600', '700'],
+    // Optional: Add display setting
+    display: 'swap',
+});
+
 const RootLayout = async ({ children }: { children: React.ReactNode }) => (
-  <html lang="en">
+  <html lang="en" className={figtree.className + " bg-[#F5F5F5]"}>
     <body>{children}</body>
   </html>
 );
